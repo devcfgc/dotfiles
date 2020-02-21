fe5420b7123cf6e32233f1df27f51fc1458436e0
devcfgc <cfgc2828@gmail.com>
2020-02-21 17:59:29 +0100

    Add aliases to zsh

--- updatedotfiles.sh
+++ updatedotfiles.sh
@@ -5,4 +5,5 @@ set -e
 echo "## COPY DOTFILES TO HOME ##"
 cp {.aliases,.profile,.tmux.conf,.zshrc} $HOME \
     && echo -e "\nsource <(kubectl completion bash)" >> ~/.bashrc \
+    && echo -e "\nsource $HOME/.aliases" >> ~/.zshrc \
     && echo -e "\nsource <(kubectl completion zsh)" >> ~/.zshrc
